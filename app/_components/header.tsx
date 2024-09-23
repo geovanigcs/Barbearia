a06ea958be140e803f5997d9437cc94d842cc7fb
geovanigcs <geovanigcs.dev@gmail.com>
2024-09-23 17:09:48 -0300

    feat: add Welcome message

---
+++ app/_components/header.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+export const Header = () => {
+    return (
+        <Card>
+            <CardContent className="px-5 py-5 justify-between items-center flex flex-row">
+                <Image src="/Logo.png" alt={"FSW Barber"} height={22} width={120} />
+                <Button variant="outline" size="icon">
+                    <MenuIcon size={22}/>
+                </Button>
+            </CardContent>
+        </Card>
+    )
+}
