49ace24c3f89c0acd645ff546bbc2a968d89d314
geovanigcs <geovanigcs.dev@gmail.com>
2024-10-18 17:24:03 -0300

    Style: add style

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -1,12 +1,38 @@
-
+import { Button } from '@/app/_components/ui/button'
+import { Card, CardContent } from '@/app/_components/ui/card'
+import { Service } from '@prisma/client'
+import Image from "next/image"
 
 interface ServiceItemProps {
     service: Service
 }
 
-export const ServiceItem = ({service}: ServiceItemProps) => {
+export const ServiceItem = ({ service }: ServiceItemProps) => {
     return (
-        <h1>{service.name}</h1>
+        <Card>
+            <CardContent className='p-3 w-full'>
+                <div className="flex gap-4 item-center w-full">
+                    <div className="relative min-h-[110px] min-w-[110px] max-w-[110px]">
+                        <Image className='rounded-lg' src={service.imageUrl} fill style={{objectFit: "contain"}} alt={service.name}/>
+                    </div>
+                    <div className="flex flex-col w-full">
+                        <h2 className="font-bold">{service.name}</h2>
+                        <p className="text-sm text-gray-400">{service.description}</p>
+                        <div className="flex item-center justify-between mt-3">
+                            <p className="text-primary text-sm font-bold">{Intl.NumberFormat(
+                                'pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL',
+                                    minimumFractionDigits: 2,
+                                    maximumFractionDigits: 2,
+                                }
+                            ).format(Number(service.price))}</p>
+                            <Button variant="secondary">Reservar</Button>
+                        </div>
+                    </div>
+                </div>
+            </CardContent>
+        </Card>
     )
 }
  
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,7 +1,6 @@
-import { Button } from "@/app/_components/ui/button"
 import { db } from "@/app/_lib/prisma"
-import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react"
 import { BarbershopInfo } from "./_components/barbershop-info"
+import { ServiceItem } from "./_components/service-item"
 
 interface BarbershopsDetailPageProps {
     params: {
@@ -17,12 +16,22 @@ const BarbershopsDetailPageProps = async ({ params }: BarbershopsDetailPageProps
         where: {
             id: params.id,
         },
+        include: {
+            services: true,
+        },
     })
     if (!barbershop){
         return null
     }
     return (
-        <BarbershopInfo barbershop={barbershop}/>
+        <div>
+            <BarbershopInfo barbershop={barbershop}/>
+            <div className="px-5 flex flex-col gap-3">
+                {barbershop.services.map((service) => (
+                    <ServiceItem key={service.id} service={service} />
+                ))}
+            </div>
+        </div>
     )
 }
 
