35770f31419194cc69873912ce23338e29b82f8d
geovanigcs <geovanigcs.dev@gmail.com>
2025-02-17 23:54:48 -0300

    Style: paragraph editing

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return (
         <div className="w-full bg-secondary py-6 px-5">
-            <p className="text-gray-400 text-xs font-bold opacity-75">© Geovani Cordeiro 2024 Copyright Gigio's Barber</p>
+            <p className="text-gray-400 text-xs font-bold opacity-75">© Geovani Cordeiro 2024 Copyright Gigio&apos;s Barber</p>
         </div>
 
 
